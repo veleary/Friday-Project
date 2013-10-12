81f8c24caca057022ef8619cadd093d2fb66166d
veleary <veleary@gmail.com>
2013-10-12 15:07:01 -0500

    Update controller for ingredient and homepage to add new item-function is not completely working

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,11 @@
 class IngredientsController < ApplicationController
   before_action :set_menu_item, only: [:show, :edit, :update, :destroy]
+  def create
+    @menu_item = MenuItem.find(params[:menu_item_id])
+
+    @ingredient = @menu_item.Ingredients.new(ingredient_params)
+  end
+  
 
   def show
     @ingredient = Ingredient.find(params[:id])
